b63ebea7d1c3e41dbe2f61c5840f993ad803318b
Breyten Ernsting <bje@dds.nl>
2022-03-14 17:49:05 +0100

    Add a script to split the pdf files.

---
+++ src/covid19.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cd /memorious
+#memorious run covid19
+cd /data/results/covid19
+for i in *.json; do
+  fn=`basename $i .json`
+  if [ ! -d "$fn" -a -s "$fn.data.pdf" ]; then
+    echo "Should make directory $fn now"
+    mkdir -p $fn
+    cd $fn
+    pdfseparate "../$fn.data.pdf" "%d.pdf"
+    cd ..
+  fi
+done
